0e317034ad54a75d3b27370f01517b9baaa9e17d
azovsergej8-sketch <azovsergej8@gmail.com>
2026-06-04 11:22:34 +0300

    Update tb_transaction.sv

--- tb/tb_transaction.sv
+++ tb/tb_transaction.sv
@@ -1,4 +1,21 @@
 class crypto_transaction;
-  rand logic[63:0] data;
-  rand logic[79:0] key;
+  rand logic[31:0] data;
+  rand logic[7:0] addr;
+  rand logic[7:0] mem;
+  logic[7:0] all_addr[] = {8'h00, 8'h01, 8'h02, 8'h03, 8'h04, 8'h05, 8'h06};
+  logic[7:0] avalaible_addr[$];
+
+  function new();
+    avalaible_addr = all_addr;
+  endfunction
+  
+  constraint addr_c{
+    addr inside {avalaible_addr};
+  };
+
+  function post_randomize();
+    int idx = avalaible_addr.find_first_index(x) with (x == tr_type);
+    if(idx >= 0) avalaible_addr.delete(idx);
+    else avalaible_addr = all_addr;
+  endfunction
 endclass
