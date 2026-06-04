2993cd84998b36e30e4347111b441fb90c4c216f
azovsergej8-sketch <azovsergej8@gmail.com>
2026-06-04 12:14:45 +0300

    Update tb_cache_driver.sv

--- tb/tb_cache_driver.sv
+++ tb/tb_cache_driver.sv
@@ -1,11 +1,11 @@
-class cache_driver;
+class crypto_driver;
   mailbox #(crypto_transaction) tr_fifo;
   
   function new( mailbox #(cache_transaction) addr_fifo);
     this.tr_fifo = addr_fifo;
   endfunction
   
-  task drive(virtual core_intf cr_intf, event clk_e);
+  task drive(virtual core_intf.core cr_intf, event clk_e);
     cache_transaction tr;
     repeat(50) begin
       if(tr == null) begin
@@ -13,13 +13,18 @@ class cache_driver;
         if(!tr.randomize()) $error("Randomization failed!");
       end
       @(clk_e);
-      if(cr_intf.ready_core) begin
+      if(cr_intf.ready) begin
         cr_intf.core_addr <= #1 tr.addr;
-        cr_intf.core_valid <= #1 1;
+        cr_intf.sel <= #1 1;
+        cr_intf.enable <= #1 1;
+        cr_intf.write <= #1 1;
         tr_fifo.put(tr);
-        tr.take_addr(tr.addr);
         tr = null;
-      end else cr_intf.core_valid <= 0;
+      end else begin
+        r_intf.sel <= 0;
+        cr_intf.enable <= 0;
+        cr_intf.write <= 0;
+      end
     end
   endtask
 endclass
