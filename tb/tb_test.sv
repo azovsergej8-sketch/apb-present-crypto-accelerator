4f4a8ffbba1f21f2e70abefdc92b15be8d60a992
azovsergej8-sketch <azovsergej8@gmail.com>
2026-06-04 13:27:27 +0300

    Update tb_test.sv

--- tb/tb_test.sv
+++ tb/tb_test.sv
@@ -1,24 +1,21 @@
 class test;
   virtual core_intf cr_if;
-  virtual memory_intf mem_if;
-  cache_driver    drv;
-  cache_checker   chk;
-  cache_mem_model mem;
+  virtual crypto_intf cryp_if;
+  crypto_driver    drv;
+  crypto_checker   chk;
   event clk_ev;
 
-  function new(virtual core_intf c_if, virtual memory_intf m_if,  mailbox #(cache_transaction) addr_fifo, event clk_e);
+  function new(virtual core_intf c_if,  virtual crypto_intf crp_if,  mailbox #(crypto_transaction) addr_fifo, event clk_e);
     this.cr_if = c_if;
-    this.mem_if = m_if;
+    this.cryp_if = crp_if;
     this.drv = new(addr_fifo);
-    this.mem = new();
     this.clk_ev = clk_e;
-    this.chk = new(addr_fifo, this.mem, this.clk_ev);
+    this.chk = new(addr_fifo, this.clk_ev);
   endfunction
 
   task run();
     fork
       drv.drive(cr_if, clk_ev);
-      mem.run_mem_responder(mem_if, clk_ev);
       chk.true_model(cr_if);
     join_any
     disable fork;
